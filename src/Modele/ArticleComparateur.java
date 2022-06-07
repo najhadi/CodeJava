f96f03fc9a3c1bedfa22f331e7208a23ca417e7d
najhadi <najoua_14@hotmail.fr>
2022-06-06 21:33:30 -0400

    ajouter fonction

--- src/Modele/ArticleComparateur.java
+++ src/Modele/ArticleComparateur.java
@@ -6,8 +6,9 @@ public class ArticleComparateur implements Comparator<Article> {
     @Override
     public int compare(Article o1, Article o2) {
         if(o1.getPrix()<o2.getPrix())
-        {
+        {System.out.println("l");
             return 1;
+
         } else if (o1.getPrix()>o2.getPrix())
         {
             return 1;
